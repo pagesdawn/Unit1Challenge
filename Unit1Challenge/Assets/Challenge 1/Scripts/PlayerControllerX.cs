7d2fb1552a31a975416639877d7d28ad41c8e324
Christian Familia <cfamilia0913@gmail.com>
2023-04-20 13:31:14 -0400

    All Bugs Fixed

--- Unit1Challenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Unit1Challenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 10f;
     public float rotationSpeed;
     public float verticalInput;
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +28,7 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.up * rotationSpeed * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+
     }
 }
